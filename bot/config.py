6d18f0c4eb395eff8189a8f2196be21ae29ff79e
Denis Travis <54004935+denistravis@users.noreply.github.com>
2020-10-23 20:22:56 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -65,7 +65,7 @@ class Messages:
     
     INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
     
-    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
+    AUTH_TEXT = "⛓️ **Hi I'm Gdrive uploader Bot, To Authorize me upload to your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here, First join my channel for updates @gdriveuploader__"
 
     DOWNLOAD_TG_FILE = "📥 **Downloading File...**\n**Filename:** ```{}```\n**Size:** ```{}```\n**MimeType:** ```{}```"
     
